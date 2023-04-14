ac86a73b3d3efec97e817b0c2372e31ecf0c57bc
Tabris <tabris.xiao@gmail.com>
2023-04-13 18:08:07 -0700

    Removed the mlir dependency

--- run_lit_test.bat
+++
@@ -1,5 +0,0 @@
-cls
-cmake -Bbuild
-cmake --build build --parallel
-.\build\bin\mc-opt.exe -canonicalize compiler\opt\lit\AA_canonicalizer.mlir
-.\build\bin\mc-opt.exe --aa-association-abstract compiler\opt\lit\AA_association.mlir
