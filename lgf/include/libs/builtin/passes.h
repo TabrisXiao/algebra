fab08a429157a474a1fe4aeaa0eb21ef06a72910
tabris <tabris.xiao@gmail.com>
2024-04-27 23:44:52 -0700

    Move few definitions to source file

---
+++ lgf/include/libs/builtin/passes.h
@@ -0,0 +1,38 @@
+
+#ifndef LGF_BUILTIN_PASSES_H
+#define LGF_BUILTIN_PASSES_H
+#include "lgf/group.h"
+#include "ops.h"
+
+namespace lgf
+{
+
+    class normalizationPass : public passBase
+    {
+    public:
+        normalizationPass() : passBase("normalization") {}
+        virtual resultCode run()
+        {
+            painter p(get_graph());
+            add_rewriter<groupRewriter<normalizer>>();
+            remove_unused_ops(get_graph());
+            remove_identical_ops(p, get_graph());
+            resultCode code = apply_rewriter_greedy(p, get_graph());
+            remove_unused_ops(get_graph());
+            remove_identical_ops(p, get_graph());
+            get_graph()->clean();
+            return code;
+        }
+
+        void remove_trivial_op(node *op);
+
+        void remove_unused_ops(graph *g);
+
+        bool remove_identical_ops(painter p, graph *g);
+    };
+
+    std::unique_ptr<passBase>
+    createNormalizationPass();
+} // namespace lgf
+
+#endif
---
+++ lgf/src/lgf/libs/builtin/normalization.cpp
@@ -0,0 +1,138 @@
+
+#include "lgf/node.h"
+#include "lgf/pass.h"
+#include "libs/builtin/passes.h"
+
+namespace lgf
+{
+    std::unique_ptr<passBase> createNormalizationPass()
+    {
+        return std::make_unique<normalizationPass>();
+    }
+
+    void normalizationPass::remove_trivial_op(node *op)
+    {
+        if (op->is_deprecate())
+            return;
+        if (op->is_trivial())
+        {
+            for (auto &h : op->get_user_handles())
+            {
+                if (h.is_coupled())
+                {
+                    auto node = h.get_dual_node();
+                    remove_trivial_op(node);
+                }
+            }
+            if (!op->get_user_size())
+            {
+                op->erase();
+            }
+        }
+    }
+
+    void normalizationPass::remove_unused_ops(graph *g)
+    {
+        for (auto &op : g->get_nodes())
+        {
+            if (auto subg = dynamic_cast<graph *>(op))
+            {
+                remove_unused_ops(subg);
+            }
+            else
+                remove_trivial_op(op);
+        }
+    }
+
+    bool normalizationPass::remove_identical_ops(painter p, graph *g)
+    {
+        // using breadth first walk to remove identical ops
+        // to avoid the case that replace of the early ops makes the later
+        // ops identical.
+        // assignID is necessary for the checkIfIdenticalExist function as the id is used to check if two ops are identical
+
+        bool changed = false;
+        if (!g)
+            return false;
+        g->assign_id(0);
+
+        auto list = g->get_nodes();
+        std::queue<node *> queue;
+        for (auto op : list)
+        {
+            if (op->is_deprecate())
+                continue;
+            if (op->get_input_size() != 0)
+                continue;
+            // skip non-trivial op, assuming no duplicate in declOp
+            if (op->is_trivial())
+            {
+                queue.push(op);
+            }
+        }
+
+        while (queue.size() > 1)
+        {
+            auto op = queue.front();
+            queue.pop();
+            while (op == queue.front())
+            {
+                queue.pop();
+            }
+            op->set_exploration(true);
+            // checking if the duplicate op exists and
+            // replace it if so.
+            lgf::node *mark = nullptr;
+            while (queue.size() > 1)
+            {
+                if (queue.front()->is_deprecate())
+                {
+                    queue.pop();
+                    continue;
+                }
+                mark = queue.front();
+                break;
+            }
+            if (mark)
+            {
+                auto target = op->get_op_represent();
+                do
+                {
+                    auto checkop = queue.front();
+                    queue.pop();
+                    if (checkop == op || checkop->is_deprecate())
+                        continue;
+                    if (target == checkop->get_op_represent())
+                    {
+                        if (mark == checkop)
+                            mark = queue.front();
+                        checkop->replace_by(op);
+                        checkop->erase();
+                        changed = true;
+                    }
+                    else
+                    {
+                        queue.push(checkop);
+                    }
+                } while (queue.front() != mark && !queue.empty());
+            }
+            for (auto &h : op->get_user_handles())
+            {
+                if (!h.is_coupled())
+                    continue;
+                auto user = h.get_dual_node();
+                if (user->is_deprecate() || user->is_explored() || !user->is_dependency_fullfilled())
+                    continue;
+                if (user->is_trivial())
+                {
+                    queue.push(user);
+                }
+            }
+        }
+        for (auto node : list)
+        {
+            node->set_exploration(false);
+        }
+        return changed;
+    }
+} // namespace lgf
